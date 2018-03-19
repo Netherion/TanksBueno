e07a62ce4b7990136d4e2dfd32d760db88b894a4
Unai Quesada <unaiques917@gmail.com>
2018-03-19 12:33:02 +0100

    Añadir cosas que no se si estan bien o no equisde

--- core/src/com/mygdx/game/Tanks.java
+++ core/src/com/mygdx/game/Tanks.java
@@ -10,17 +10,24 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Affine2;
+import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+
+import javax.xml.soap.Text;
 
 public class Tanks extends ApplicationAdapter {
 
@@ -37,29 +44,36 @@ public class Tanks extends ApplicationAdapter {
 
 		return skin;
 	}
-	
-	@Override
-	public void create () {
 
-		TextButton boton = new TextButton("opciones", getSkin());
+	public void dibujarMain()
+	{
+		TextButton boton = new TextButton("boton 1", getSkin());
 
-		boton.addListener(new InputListener() {
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				// Your code here
-				return false;
-			}
-		});
+		TextButton boton2 = new TextButton("boton 2", skin);
+		int h = Gdx.graphics.getHeight();
+		int w = Gdx.graphics.getWidth();
 
 		stage = new Stage();
 		table = new Table(getSkin());
 
-		table.setFillParent(true);
+		table.setPosition(w / 2, h / 2 - 80);
 		stage.addActor(table);
 
+
+		Table tabla2 = new Table(getSkin());
+
+		tabla2.setPosition(w/2, h - 20);
+
+
 		table.add(boton);
+		table.row();
+		table.add(boton2).space(50,0,0,0);
+	}
+	
+	@Override
+	public void create () {
 
-
+		dibujarMain();
 
 	}
 	public void resize (int width, int height) {
